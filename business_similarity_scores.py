c81a2a3fde998ab90904a4d0d70c9d8eb86d6808
Alex Maiorella <35647226+donkeyteethUX@users.noreply.github.com>
2018-05-22 16:26:20 -0500

    worked on formulas

--- business_similarity_scores.py
+++ business_similarity_scores.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import json
 from math import radians, cos, sin, asin, sqrt
+import re
 
 
 # with open("data/categories.json") as f:
@@ -28,16 +29,16 @@ from math import radians, cos, sin, asin, sqrt
 
 def calculate_haversine_distance(lon1, lat1, lon2, lat2):
     """
-    Calculate the haversine distance in miles between two businesses 
+    Calculate the haversine distance in miles between two businesses
     """
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    c = 2 * asin(sqrt(a))
     r = 3956 # radius of earth in miles
     return c * r
 
@@ -56,16 +57,47 @@ def calculate_stars_sim(rating1, rating2):
 #     for cat_list in (categories1, categories2):
 #         for cat in cat_list:
 
+def find_hours(business_id, samp_hours):
+    for i in samp_hours:
+        if i[0] == business_id:
+            hours = []
+            for j in range(1,8):
+                start, end = i[j].split("-")
+                start_hrs_mins = re.match("([0-9]*):?([0-9]*)", start)
+                start_frac = int(start_hrs_mins.group(2)) / 60
+                end_hrs_mins = re.match("([0-9]*):?([0-9]*)", end)
+                end_frac = int(end_hrs_minutes.group(2)) / 60
+                fixed_start = int(start_hrs_mins.group(1)) + str(start_frac)
+                fixed_end = int(end_hrs_mins.group(1)) + str(end_frac)
+                hours.append((fixed_start, fixed_end))
+            return hours
 
-def go(filename):
-    sample = pd.read_csv(filename, usecols = ['business_id', 'latitude',
-        'longitude', 'stars', 'review_count', 'categories'])[:100]
+def hours_overlap(id1, id2):
+    h1 = find_hours(id1)
+    h2 = find_hours(id2)
+    overlap = 0
+    h1_hours = 0
+    for day in range(7):
+        h1_hours += h1[day][1] - h1[day][0]
+        both_open = min(h1[day][1] - h2[day][0], h2[day][1] - h1[day][0])
+        if both_open > 0:
+            overlap += both_open
+    return overlap / h1_hours
+
+def go(path):
+    sample_business = pd.read_csv(path + "yelp_business_samp.csv", usecols = ['business_id', 'latitude',
+        'longitude', 'stars', 'review_count', 'categories'])
+
+    sample_hours = pd.read_csv(path + "yelp_hours_samp.csv")
+
+    sample_attributes = pd.read_csv(path + "yelp_business_attributes_samp.csv")
 
     rows_list = []
 
     for row1 in sample.itertuples():
         for row2 in sample.itertuples():
             if row1[0] != row2[0]:
+                hours_overlap = hours_overlap(row1[0], row2[0])
                 distance = calculate_haversine_distance(row1[3], row1[2], row2[3], row2[2])
                 stars_sim = calculate_stars_sim(row1[4], row2[4])
                 review_count_sim = abs(row1[5] - row2[5]) / min(row1[5], row2[5])
@@ -74,13 +106,13 @@ def go(filename):
                 category_sim = (len(set(categories1).intersection(set(categories2))) \
                     / (len(categories1) + len(categories2))) + 1
                 # added 1 to deal with scores of 0 turning sim score into 0
-                sim_score = (1 / distance) * stars_sim * review_count_sim * category_sim
-                rows_list.append({'business_id1': row1[1], 
+                sim_score = (1 / distance) * stars_sim * review_count_sim * category_sim * hours_overlap
+                rows_list.append({'business_id1': row1[1],
                     'business_id2': row2[1], 'similarity': sim_score})
 
     return pd.DataFrame(rows_list)
 
 
 if __name__ == "__main__":
-    output_df = go('data/yelp_business.csv')
+    output_df = go("C:/Users/alex/Desktop/yelp_data/yelp-dataset/")
     output_df.to_csv('data/yelp_business_similarity_scores_sample.csv', index = False)
--- business_success_scores.py
+++ business_success_scores.py
@@ -1,19 +1,38 @@
 import pandas as pd
+import sys
 
+def find_time_open(id, sample_reviews):
+    for l in sample_reviews:
+        earliest = None
+        latest = None
+        if l[2] == id:
+            date = time.strptime(l[4], "%Y-%m-%d")
+            if date < earliest:
+                earliest = date
+            if date > latest:
+                latest = date
 
-def go(filename):
-    sample = pd.read_csv(filename, usecols = ['business_id', 'latitude',
-        'longitude', 'stars', 'review_count', 'categories'])[:100]
+def go(path):
+
+    sample_business = pd.read_csv(path + "yelp_business_samp.csv", usecols = ['business_id', 'latitude',
+        'longitude', 'stars', 'review_count', 'categories'])
+
+    sample_reviews = pd.read_csv(path + "yelp_review_samp.csv")
 
     rows_list = []
 
-    for row in sample.itertuples():
-        success_score = row[4] * row[5]
+    for row in sample_business.itertuples():
+
+        success_score = row[3] * row[4]
         rows_list.append({'business_id': row[1], 'success': success_score})
 
     return pd.DataFrame(rows_list)
 
 
 if __name__ == "__main__":
-    output_df = go('data/yelp_business.csv')
+    output_df = go("C:/Users/alex/Desktop/yelp_data/yelp-dataset/")
     output_df.to_csv('data/yelp_business_success_scores_sample.csv', index = False)
+
+# C:/Users/alex/Desktop/yelp_data/yelp-dataset/yelp_business_hours.csv
+# C:/Users/alex/Desktop/yelp_data/yelp-dataset/yelp_business_attributes.csv
+# C:/Users/alex/Desktop/yelp_data/yelp-dataset/yelp_review.csv
