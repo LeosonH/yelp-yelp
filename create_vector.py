0f79d36520f6bd4e53cfd541dbd3c0e75b2bcf98
Yuqian Gong <yuqian919@uchicago.edu>
2018-06-01 13:56:35 -0500

    prepare_vector_for_dict

--- create_vector.py
+++ create_vector.py
@@ -5,18 +5,24 @@ from mrjob.step import MRStep
 from sklearn.feature_extraction import stop_words
 import re
 from gensim.corpora import Dictionary
+#from mr3px.csvprotocol import CsvProtocol
 
 stopw = list(stop_words.ENGLISH_STOP_WORDS)
 stopw.extend(['yelp', 'got', 'does', 'quite','going','just', 'right'])
 dictionary = Dictionary()
+l = list()
 
 
 class create_base_vector(MRJob):
+    #OUTPUT_PROTOCOL = CsvProtocol
     def mapper(self, _, line):
         doc = []
-        review_list = line.split(',')
+        #review_list = line.split('|')
+        review_list = next(csv.reader([line], delimiter = '|'))
         review_text = review_list[-1]
+        biz_id = review_list[1]
         # Check for both header rows and blank rows
+        #print(review_list)
         if len(re.findall('.*text$', review_text)) == 0 and not re.match(r'^\s*$', review_text):
             doc = re.sub("[^\\w\\s]", "", review_text)
             doc = re.sub(r"\b\d+\b","", doc)
@@ -24,18 +30,49 @@ class create_base_vector(MRJob):
             # remove stop words
             doc = [x for x in doc if x not in stopw]
             doc = list(set(doc))
-        yield None, doc         
+            yield biz_id, doc 
 
-    def reducer_init(self):
-        self.l = list()
+    #def combiner(self, biz_id, docs):
+       #docs = list(docs)
+       # docs = [word for word_l in docs for word in word_l]
+       # docs = list(set(docs))
+       # yield biz_id, docs
 
-    def reducer(self, _, doc):
+
+    #def reducer_init(self):
+        #self.l = list()
+
+    def reducer(self, biz_id, docs):
+        docs = list(docs)
+        docs = [word for word_l in docs for word in word_l]
+        docs = list(set(docs))
+        #print(docs)
+        #self.l.append(docs)
+        yield None, docs
+
+
+    def reducer_more(self, _, docs):
+        docs = list(docs)
+        yield None, docs 
+
+    def steps(self):
+        return[
+                MRStep(mapper = self.mapper,
+                    reducer = self.reducer),
+                MRStep(reducer = self.reducer_more)
+
+        ]
+
+
+    #def reducer_final(self):
+        #yield None, list(self.l)
+
     	# return generator to list form
-        self.l.append(list(doc))
-        for i in self.l:
-        	dictionary.add_documents(i)
+        
+        #for i in self.l:
+        	#dictionary.add_documents(i)
         # replace directory path with your own
-        dictionary.save("C:/Users/leoso/Desktop/Uchicago Year 1/Spring/git/yelp-yelp/base_vector.dict")
+        #dictionary.save("/Users/Nancygong/Documents/Downloads/Nancy Gong/芝加哥/CS3/yelp project/dataset/base_vector.dict")
     
     
 if __name__ == '__main__':
