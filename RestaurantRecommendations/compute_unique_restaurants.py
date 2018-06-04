c0e1e1156f2c7c44d0ab7aa55f557f47313ab87f
Leoson Hoay <leoson@cs.uchicago.edu>
2018-06-04 05:20:14 -0500

    cleaning repo

--- RestaurantRecommendations/compute_unique_restaurants.py
+++ RestaurantRecommendations/compute_unique_restaurants.py
@@ -5,12 +5,11 @@
 #
 #-------------------------------------------------------------------------------
 # Given a dataset of user-paired restaurants, and a dataset of paired most-similar users, 
-# find potential recommendable restaurants for each user and their similarity score and 
-# haversine distance.
+# find potential recommendable restaurants for each user and their similarity score and haversine
+# distance.
 #
-# To run: python3 compute_unique_restaurants.py -r dataproc --num-core-instances 7 
-# --instance-type n1-highmem-2 --file reviews_dictionary.dict --file reviews_corpus.mm
-# --file user_rest_pair.csv similar_user.txt > user_sim_rest_pair.txt
+# To run: python3 compute_unique_restaurants.py -r dataproc --file user_rest_pair.csv 
+# --num-core-instances 7 similar_users.txt > user_sim_rest_pair.txt
 
 from mrjob.job import MRJob
 import csv
@@ -43,8 +42,15 @@ def haversine_distance(restaurant1, restaurant2):
 
 	return d
 
+
 class compute_unique(MRJob):
-	
+	'''
+	Given a dataset of reviews, and a dataset of paired most-similar users, 
+	find potential recommendable restaurants for each user.
+
+	Yields the user's id, the most similar user's id, and a recommendable 
+	restaurant from the latter.
+	'''
 	def mapper_init(self):
 		'''
         Load required files and models here.
@@ -80,6 +86,28 @@ class compute_unique(MRJob):
 				sim_score = cossim(lsi1, lsi2)
 				dist = haversine_distance((la, lon), (sim_la, sim_lon))
 				yield None, (user, sim_user, rest, sim_rest, sim_score, dist)
+
+		'''
+		self.s = set()
+		self.s2 = set()
+		read_line = next(csv.reader([line]))
+		sim_user = read_line[1]
+		user = read_line[2]
+
+		for i in range(len(review_restaurant_df)):
+			if pd.isnull(review_restaurant_df.iloc[i]["text"]) == False:
+				if review_restaurant_df.iloc[i]["user_id"] == user:
+					self.s.add(review_restaurant_df.iloc[i][1])
+				if review_restaurant_df.iloc[i]["user_id"] == sim_user:
+					self.s2.add(review_restaurant_df.iloc[i][1])
+
+		# remove overlapping restaurants
+		l3 = [x for x in list(self.s2) if x not in list(self.s)]
+
+		for j in l3:
+			for k in self.s:
+				yield (user, sim_user), (k, j)
+		'''
 
 if __name__ == '__main__':
     compute_unique.run()
